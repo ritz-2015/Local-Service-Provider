a53cec853e392c6cae7d1398700cc66e6313e8d7
ritz-2015 <ritzty13@gmail.com>
2025-06-17 17:20:24 +0530

    Users table created

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app=express();
+const db=require('./models')
 
-
-app.listen(3001,()=>{
+db.sequelize.sync().then(()=>{    //create all the tables
+    //whenever we run server it starts from here
+    app.listen(3001,()=>{              
     console.log("Server running on port 3001")
+    });
 });
+
---
+++ backend/models/users.js
@@ -0,0 +1,26 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Users=sequelize.define("Users",{
+        name:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: { isEmail: true },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.ENUM("customer", "provider"),
+            allowNull: false,
+        }
+    }); //Users-table name
+    // name, email,password,role-attributes
+    
+
+    return Users
+};
